645652aaabea5cf1ab89e760cc2bb23d9574614d
Thomas Ko <ThomasKo@calvisitor-10-105-150-174.calvisitor.1918.berkeley.edu>
2017-12-09 12:52:15 -0800

    location file

---
+++ public/js/updatelocation.js
@@ -0,0 +1,33 @@
+
+    var locations = [
+      ['House0', 37.79839089938072,-122.41241455078125,'1617 Mason St, San Francisco, CA 94133, USA'],
+      ['House1', 37.7945928242851,-122.43988037109375,'2545-2599 Vallejo St, San Francisco, CA 94123, USA'],
+      ['House2', 37.72103447742806,-122.45704650878906,'154 Granada Ave, San Francisco, CA 94112, USA'],
+      ['House3', 37.73922729512254,-122.45464324951172,'999 Rockdale Dr, San Francisco, CA 94127, USA'],
+      ['House4', 37.742756727055514,-122.41447448730469,'899 Bernal Heights Blvd, San Francisco, CA 94110, USA'],
+      ['House5', 37.772682258736,-122.42013931274414,'79 Brady St, San Francisco, CA 94103, USA'],
+      ['House6', 37.75090092768723,-122.48382568359375,'1899 26th Ave, San Francisco, CA 94122, USA'],
+      ['House7', 37.72375010711357,-122.47489929199219,'3500-3598 19th Ave, San Francisco, CA 94132, USA'],
+      ['House8', 37.784554114444994,-122.45464324951172,'78-82 Parker Ave, San Francisco, CA 94118, USA'],
+      ['House9', 37.772682258736,-122.42013931274414,'956 Valencia St, San Francisco, CA 94110, USA'],
+      ['House10', 37.75741564287944,-122.42134094238281,'956 Valencia St, San Francisco, CA 94110, USA'],
+      ['House11', 37.74682893940135,-122.43473052978516,'23 Kronquist Ct, San Francisco, CA 94131, USA'],
+      ['House12', 37.76257271890944,-122.4700927734375,'1368 Funston Ave, San Francisco, CA 94122, USA'],
+      ['House13', 37.756872771856465,-122.46116638183594,'400 Warren Dr, San Francisco, CA 94131, USA'],
+      ['House14', 37.75035800886177,-122.50099182128906,'1947-1951 42nd Ave, San Francisco, CA 94116, USA'],
+      ['House15', 37.74954362315467,-122.47764587402344,'2035 20th Ave, San Francisco, CA 94116, USA'],
+      ['House16', 37.78265474565738,-122.42546081542969,'1090 Eddy, 1090 Eddy St, San Francisco, CA 94109, USA'],
+      ['House17', 37.76420119453823,-122.44571685791016,'175-179 Clifford Terrace, San Francisco, CA 94117, USA'],
+      ['House18', 37.754972691904946,-122.48554229736328,'1743 27th Ave, San Francisco, CA 94122, USA'],
+      ['House19', 37.74872922848495,-122.48210906982422,'1500-1598 Quintara St, San Francisco, CA 94116, USA'],
+      ['House20', 37.78591077655136,-122.48485565185547,'196 25th Ave, San Francisco, CA 94121, USA'],
+      ['House21', 37.77532815168286,-122.46253967285156,'702 5th Ave, San Francisco, CA 94118, USA'],
+      ['House22', 37.779127216982424,-122.40966796875,'573 Minna St, San Francisco, CA 94103, USA'],
+      ['House23', 37.750358008861776,-122.48828887939453,'1979 30th Ave, San Francisco, CA 94116, USA'],
+      ['House24', 37.77695634643178,-122.5033950805664,'5135 Anza St, San Francisco, CA 94121, USA'],
+      ['House25', 37.70772645289049,-122.43301391601562,'198 Canyon Dr, Daly City, CA 94014, USA'],
+      ['House26', 37.70147900486174,-122.48966217041016,'23 Fleetwood Dr, Daly City, CA 94015, USA'],
+      ['House27', 37.73135334055843,-122.39662170410156,'376 Thornton Ave, San Francisco, CA 94124, USA'],
+    ];
+ 
+
